279e0f7f78f5e2602d6c98c9fceb0c4c916293c8
DAMIANGW <kfa92893@gmail.com>
2025-02-27 05:39:07 +0000

    Create User.py

---
+++ Backend/models/User.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel, EmailStr
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+# Pydantic Schemas
+class UserCreate(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    username: str
+
+    class Config:
+        orm_mode = True
